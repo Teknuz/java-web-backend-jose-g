89f41b2d199d4a66ae01a69ca3fb03323505d4c1
Teknuz <jg323891@gmail.com>
2026-03-24 08:16:13 -0300

    record and class separated by diferente url

--- src/main/java/py/edu/ucsa/java_web_backend_jose/controllers/BackendDataController.java
+++ src/main/java/py/edu/ucsa/java_web_backend_jose/controllers/BackendDataController.java
@@ -1,5 +1,6 @@
 package py.edu.ucsa.java_web_backend_jose.controllers;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +11,39 @@ import py.edu.ucsa.java_web_backend_jose.dto.BackendDataRecord;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/api/v1/backend/data")
+@RequestMapping("/api/v1/backend/data/")
 public class BackendDataController {
-
     @GetMapping("/full")
-    public ResponseEntity<?> getBackendData(){
-        BackendData data = new BackendData();
-        BackendDataRecord dataRecord = new BackendDataRecord("JAVA WEB BACKEND JOSEG","0.0.0-SNAPSHOT",LocalDate.now());
+    public ResponseEntity<?> getBackendDataGeneric(){
+        BackendData data =new BackendData();
         data.setProjectName("JAVA WEB BACKEND JOSEG");
         data.setProjectVerion("0.0.0-SNAPSHOT");
         data.setProjectVersionDate(LocalDate.now());
+        return ResponseEntity.ok(data);
+    }
+
+    @GetMapping("/class")
+    public ResponseEntity<?> getBackendData(){
+        BackendData data = buildBackendData();
+        return ResponseEntity.ok(data);
+    }
+    @GetMapping("/record")
+    public ResponseEntity<BackendDataRecord> getBackendDataRecord(){
+        BackendData data = buildBackendData();
+
+        BackendDataRecord dataRecord = new BackendDataRecord(data.getProjectName(),
+                data.getProjectVerion(),
+                data.getProjectVersionDate());
 
         return ResponseEntity.ok(dataRecord);
+    }
+
+    private BackendData buildBackendData() {
+        BackendData data = new BackendData();
+        data.setProjectName("JAVA WEB BACKEND JOSEG");
+        data.setProjectVerion("0.0.0-SNAPSHOT");
+        data.setProjectVersionDate(LocalDate.now());
+        return data;
     }
 
 
